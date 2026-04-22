0ce094f7da95bed59ab90eead4a9bba91490434b
akshayyy2802 <akshaymudgal2007@gmail.com>
2026-04-22 08:38:23 +0530

    Update home.js

--- home.js
+++ home.js
@@ -1,3 +1,12 @@
 function skip() {
   window.location.href = "canteen.html";
 }
+document.addEventListener("DOMContentLoaded", function () {
+  const btn = document.getElementById("skipBtn");
+
+  if (btn) {
+    btn.addEventListener("click", function () {
+      window.location.href = "canteen.html";
+    });
+  }
+});
